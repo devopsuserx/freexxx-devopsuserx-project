aacb22705030c6f525cce4a1e45a4fa60529cc97
devopsuserx <devopsuserx@gmail.com>
2022-12-17 07:17:53 -0600

    added initial commit

---
+++ src/test/java/com/devopsuserxmaven/freexxx_devopsuserx_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.devopsuserxmaven.freexxx_devopsuserx_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
